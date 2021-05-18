6b0a07f07d45a50197a50dfc472b06041bba1da2
makhatadze <vitomaxatadze@gmail.com>
2021-05-18 11:22:55 +0400

    Make users permissions pivot table with migration.

---
+++ database/migrations/2021_05_18_071935_create_users_permissions_table.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *  database/migrations/2021_05_18_071935_create_users_permissions_table.php
+ *
+ * Date-Time: 18.05.21
+ * Time: 11:22
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('users_permissions', function (Blueprint $table) {
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('permission_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+
+            $table->primary(['user_id','permission_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users_permissions');
+    }
+}
