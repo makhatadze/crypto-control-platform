56b386d478526db1122de90fd7ff3a1e43d5997e
makhatadze <vitomaxatadze@gmail.com>
2021-05-18 13:26:37 +0400

    Add validation to login.

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,10 +1,9 @@
 @extends('admin.layout.login')
-
 @section('head')
-    <link rel="stylesheet" href="{{ asset('../assets/css/icons.css') }}" />
-    <link rel="stylesheet" href="{{ asset('../assets/css/bootstrap.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('../assets/css/style.css') }}" />
-    <link rel="stylesheet" href="{{ asset('../assets/css/responsive.css') }}" />
+    <link rel="stylesheet" href="{{ asset('../assets/css/icons.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('../assets/css/bootstrap.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('../assets/css/style.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('../assets/css/responsive.css') }}"/>
 
     <title>Login - Crypto Admin</title>
 @endsection
@@ -17,47 +16,48 @@
                     <div class="login-inner">
 
                         <div class="logo">
-                            <img src="assets/images/logo-dark.png"  alt="logo"/>
+                            <img src="/assets/images/logo-dark.png" alt="logo"/>
                         </div>
 
                         <h2 class="header-title text-center">Login</h2>
 
-                        <form>
-                            <div class="form-group">
-                                <input type="text" class="form-control"  placeholder="Username" >
+                        <form method="POST" action="{{route('login')}}">
+                            <div class="form-group @error('email') has-error @enderror">
+                                <input type="text" class="form-control" name="email" placeholder="Email">
+                                @error('email')
+                                    <div id="email-error" class="help-block animated fadeInDown">
+                                        {{$message}}
+                                    </div>
+                                @enderror
                             </div>
 
-                            <div class="form-group">
-                                <input type="text" class="form-control"  placeholder="Password" >
+                            <div class="form-group @error('password') has-error @enderror"">
+                                <input type="text" class="form-control" name="password" placeholder="Password">
+                                @error('password')
+                                <div id="password-error" class="help-block animated fadeInDown">
+                                    {{$message}}
+                                </div>
+                                @enderror
                             </div>
 
                             <div class="form-group">
                                 <div class="pull-left">
                                     <div class="checkbox primary">
-                                        <input  id="checkbox-2" type="checkbox">
-                                        <label for="checkbox-2">Remember me</label>
+
                                     </div>
                                 </div>
 
-                                <div class="pull-right">
-                                    <a href="reset-password.html" class="a-link">
-                                        <i class="fa fa-unlock-alt"></i> Forgot password?
-                                    </a>
-                                </div>
                             </div>
-
+                            @csrf
                             <div class="form-group">
-                                <input type="submit" value="Login" class="btn btn-primary btn-block" >
+                                <input type="submit" value="Login" class="btn btn-primary btn-block">
                             </div>
 
-                            <div class="form-group text-center">
-                                Don't have an account?  <a href="registration.html">Sign Up </a>
-                            </div>
 
                         </form>
 
                         <div class="copy-text">
-                            <p class="m-0">2017 &copy; Meter admin</p>
+                            <p class="m-0">2021 &copy; Insite International</p>
                         </div>
 
                     </div>
