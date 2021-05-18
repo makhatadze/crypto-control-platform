3a9ccbee0fceeff1428c69bb116ad76af7989ffc
makhatadze <vitomaxatadze@gmail.com>
2021-05-18 11:13:14 +0400

    Make roles permissions table.

---
+++ database/migrations/2021_05_18_071245_create_roles_permissions_table.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ *  database/migrations/2021_05_18_071245_create_roles_permissions_table.php
+ *
+ * Date-Time: 18.05.21
+ * Time: 11:12
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles_permissions', function (Blueprint $table) {
+            $table->bigInteger('role_id')->unsigned();
+            $table->bigInteger('permission_id')->unsigned();
+
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+
+
+            $table->primary(['role_id', 'permission_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles_permissions');
+    }
+}
