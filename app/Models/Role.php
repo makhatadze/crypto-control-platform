b9d57c9496215bb19c2447fc1cc6b3798629f135
makhatadze <vitomaxatadze@gmail.com>
2021-05-18 10:25:01 +0400

    Add role model with migration.

---
+++ app/Models/Role.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *  app/Models/Role.php
+ *
+ * Date-Time: 18.05.21
+ * Time: 10:22
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Role
+ * @package App\Models
+ *
+ * @property integer $id
+ * @property string name
+ * @property string slug
+ * @property string created_at
+ * @property string updated_at
+ */
+class Role extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'roles';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name','slug'];
+}
---
+++ database/migrations/2021_05_18_062204_create_roles_table.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *  database/migrations/2021_05_18_062204_create_roles_table.php
+ *
+ * Date-Time: 18.05.21
+ * Time: 10:22
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
