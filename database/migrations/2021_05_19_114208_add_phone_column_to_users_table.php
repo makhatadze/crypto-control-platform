80f41218cf15c35132ddebfd7196860861e63476
makhatadze <vitomaxatadze@gmail.com>
2021-05-19 15:43:03 +0400

    Add users phone column.

---
+++ database/migrations/2021_05_19_114208_add_phone_column_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *  database/migrations/2021_05_19_114208_add_phone_column_to_users_table.php
+ *
+ * Date-Time: 19.05.21
+ * Time: 15:42
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPhoneColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone')->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('phone');
+        });
+    }
+}
