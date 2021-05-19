f311da2d92dc0f35342e87a0fdc7916852e41849
makhatadze <vitomaxatadze@gmail.com>
2021-05-19 15:08:00 +0400

    Add status column to users table.

---
+++ database/migrations/2021_05_19_110616_add_status_column_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *  database/migrations/2021_05_19_110616_add_status_column_to_users_table.php
+ *
+ * Date-Time: 19.05.21
+ * Time: 15:06
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('status')->after('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+}
