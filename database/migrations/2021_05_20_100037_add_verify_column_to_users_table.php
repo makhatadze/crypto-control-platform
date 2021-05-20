1a401d7c5658ac07f46d0c1db87b9f43aefd29c0
makhatadze <vitomaxatadze@gmail.com>
2021-05-20 14:01:32 +0400

    Add verify column to users table.

---
+++ database/migrations/2021_05_20_100037_add_verify_column_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *  database/migrations/2021_05_20_100037_add_verify_column_to_users_table.php
+ *
+ * Date-Time: 20.05.21
+ * Time: 14:00
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddVerifyColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('verify')->after('status')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('verify');
+        });
+    }
+}
