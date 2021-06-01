052770a11b2d6ad213f798615c8a01f25cab998f
makhatadze <vitomaxatadze@gmail.com>
2021-06-01 10:35:03 +0400

    Make footer and header.

---
+++ resources/views/client/layout/components/footer.blade.php
@@ -0,0 +1,40 @@
+<footer class="bg-pattern darkblue ptb-100" style="background-color: #121d33;">
+    <div class="container">
+        <div class="footer">
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="footer-logo pb-25">
+                        <a href="/en"><img src="/client/images/logo.png" width="250px" alt="SafeCrypto"></a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="footer-link">
+                        <ul>
+                            <li><a href="/en">Home</a></li>
+                            <li><a href="/en/about-us">About Us</a></li>
+                            <li><a href="/en/roadmap">Roadmap</a></li>
+                            <li><a href="/en/faq">FAQ</a></li>
+                            <li><a href="/en/contact">Contacts</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="subscribe">
+                        <div class="form-group">
+                            <label>Subscribe to our Newsleter</label>
+                            <input type="email" class="form-control" placeholder="Enter your email Address">
+                            <input type="submit" name="submit" value="Subscribe" class="submit">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright">
+            <div class="row">
+                <div class="col-lg-6">
+                    <p>© SafeCrypto all Rights Reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/client/layout/components/header.blade.php
@@ -0,0 +1,28 @@
+<header class="transition fixed">
+    <div class="container">
+        <div class="row flex-align">
+            <div class="col-lg-4 col-md-3 col-8">
+                <div class="logo">
+                    <a href=""><img src="/client/images/logo.png" width="250px" class="transition" alt="SafeCrypto"></a>
+                </div>
+            </div>
+            <div class="col-lg-8 col-md-9 col-4 text-right">
+                <div class="menu-toggle">
+                    <span></span>
+                </div>
+                <div class="menu">
+                    <ul class="d-inline-block">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About Us</a></li>
+                        <li><a href="">Roadmap</a></li>
+                        <li><a href="">Contacts</a></li>
+                        <li><a href="">Sign Up</a></li>
+                    </ul>
+                    <div class="signin d-inline-block">
+                        <a href="https://my.safecrypto.cc/login" class="btn">Sign in</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
